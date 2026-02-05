8ed0feadb5df45ad138ab70970724471d2496ef7
Julien-G-Man <juliengmanana@gmail.com>
2026-02-05 21:32:06 +0100

    fix queue/stack

--- DataStructures/queues_and_stacks.py
+++ DataStructures/queues_and_stacks.py
@@ -6,22 +6,32 @@ The difference between the two is that a queue stores items in a first-in, first
 whereas stack stores items in a last-in, first-out (LIFO) format
 """
 
-# Queues follow FIFO protocol to store and remove data
 class Node:
-    def __init__(self, value):
+    def __init__(self, value, next_node=None):
         self.value = value
-        self.next = None
+        self.next_node = next_node
 
+    def get_value(self):
+        return self.value
+
+    def get_next_node(self):
+        return self.next_node
+
+    def set_next_node(self, next_node):
+        self.next_node = next_node
+
+# --- QUEUE: First-In, First-Out (FIFO) ---
 class Queue:
-    def __init__(self):
+    def __init__(self, max_size=None):
         self.head = None
         self.tail = None
         self.size = 0
+        self.max_size = max_size
   
     def enqueue(self, value):
         if self.has_space():
             item_to_add = Node(value)
-            print("Adding " + str(item_to_add.get_value()) + " to the queue!")
+            print(f"Adding {item_to_add.get_value()} to the queue!")
             if self.is_empty():
                 self.head = self.tail = item_to_add                
             else:
@@ -32,12 +42,11 @@ class Queue:
             print("Sorry, no more room!")
        
     def dequeue(self):
-        if self.get_size() > 0:
+        if not self.is_empty():
             item_to_remove = self.head
-            print(str(item_to_remove.get_value()) + " is served!")
-            if self.get_size() == 1:
-                self.head = None
-                self.tail = None
+            print(f"{item_to_remove.get_value()} is served!")
+            if self.size == 1:
+                self.head = self.tail = None
             else:
                 self.head = self.head.get_next_node()
             self.size -= 1
@@ -46,54 +55,28 @@ class Queue:
             print("The queue is totally empty!")
   
     def peek(self):
-        if self.size > 0:
+        if not self.is_empty():
             return self.head.get_value()
-        else:
-            print("No orders waiting!")
- 
+        print("No orders waiting!")
+        return None
+
     def get_size(self):
         return self.size
     
     def is_empty(self):
         return self.size == 0
 
-print("Creating a deli line with up to 10 orders...\n------------")
-deli_line = Queue()
-print("Adding orders to our deli line...\n------------")
-deli_line.enqueue("egg and cheese on a roll")
-deli_line.enqueue("bacon, egg, and cheese on a roll")
-deli_line.enqueue("toasted sesame bagel with butter and jelly")
-deli_line.enqueue("toasted roll with butter")
-deli_line.enqueue("bacon, egg, and cheese on a plain bagel")
-deli_line.enqueue("two fried eggs with home fries and ketchup")
-deli_line.enqueue("egg and cheese on a roll with jalapeos")
-deli_line.enqueue("plain bagel with plain cream cheese")
-deli_line.enqueue("blueberry muffin toasted with butter")
-deli_line.enqueue("bacon, egg, and cheese on a roll")
-deli_line.enqueue("western omelet with home fries")
+    def has_space(self):
+        if self.max_size is None:
+            return True
+        return self.max_size > self.size
 
-print("------------\n Our first order will be " + deli_line.peek())
-print("------------\n Now serving...\n------------")
-deli_line.dequeue()
-deli_line.dequeue()
-deli_line.dequeue()
-deli_line.dequeue()
-deli_line.dequeue()
-deli_line.dequeue()
-deli_line.dequeue()
-deli_line.dequeue()
-deli_line.dequeue()
-deli_line.dequeue()
-deli_line.dequeue()
-
-# Stacks follow teh LIFO protocol to store and remove data
-
-from node import Node
-
+# --- STACK: Last-In, First-Out (LIFO) ---
 class Stack:
     def __init__(self, limit=1000):
         self.top_item = None
         self.size = 0
+        self.limit = limit
         
     def push(self, value):
         if self.has_space(): 
@@ -105,22 +88,44 @@ class Stack:
             print("All out of space!")
             
     def pop(self):
-        if self.size > 0:
+        if not self.is_empty():
             item_to_remove = self.top_item
             self.top_item = item_to_remove.get_next_node()
             self.size -= 1
             return item_to_remove.get_value()
         else:
-            print("This stack is totally empty.")   
+            print("This stack is totally empty.")
+            return None
                 
     def peek(self):
-        if self.size > 0:
+        if not self.is_empty():
             return self.top_item.get_value()
-        else:
-            print("Nothing to see here!")
+        print("Nothing to see here!")
+        return None
   
     def is_empty(self):
         return self.size == 0
-    
-    
-    
+
+    def has_space(self):
+        return self.limit > self.size
+
+
+# --- TEST EXECUTION ---
+
+print("Creating a deli line with up to 10 orders...\n------------")
+deli_line = Queue(10)
+orders = [
+    "egg and cheese", "bacon egg and cheese", "toasted bagel", 
+    "roll with butter", "plain bagel", "two fried eggs", 
+    "jalapeno egg and cheese", "cream cheese bagel", 
+    "muffin", "bacon bagel", "western omelet"
+]
+
+for order in orders:
+    deli_line.enqueue(order)
+
+print(f"------------\nOur first order will be: {deli_line.peek()}")
+print("------------\nNow serving...\n------------")
+
+while not deli_line.is_empty():
+    deli_line.dequeue()
